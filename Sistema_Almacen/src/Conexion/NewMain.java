ed2c3e2407e91ba06786b2410dae332b5304094f
JhoylerCrespin <jhoybcv.01@gmail.com>
2024-07-06 00:49:52 -0500

    menudos cambios en la bd

---
+++ Sistema_Almacen/src/Conexion/NewMain.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Conexion;
+
+/**
+ *
+ * @author jhoyb
+ */
+import java.sql.*;
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws SQLException {
+        // TODO code application logic here
+        
+        conexion conec = new conexion();
+        conec.conectar();
+        ResultSet rs = conec.getConsulta("SELECT * FROM sistema_registro.empleado;");
+        
+          while (rs.next()) {
+    int id = rs.getInt(1);
+    String nombre = rs.getString(2);
+    String edad = rs.getString(3);
+
+    System.out.println("ID: " + id + ", Nombre: " + nombre + ", Edad: " + edad);
+}
+          
+         conec.cerrarConexion();
+        
+    }
+    
+}
